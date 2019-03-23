049dcef60bb9cbaaf729c96415fa98073f5f17ac
AguilarManu <38508255+AguilarManu@users.noreply.github.com>
2019-03-23 12:16:25 -0600

    Summary
    asdasd

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Paginacion</title>
+	<link rel="stylesheet" type="text/css" href="assets/bootstrapv3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/bootstrapv3/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/fontawesome5.3.1/css/all.css">
+	<link rel="stylesheet" type="text/css" href="assets/fontawesome5.3.1/css/regular.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/fontawesome5.3.1/css/fontawesome.css">
+	<link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+	<div class="container">
+		
+	<div class="row">
+            <div class="col-md-12 col-centered">
+                <div class="grid simple">
+                    <div class="grid-title">
+                        <h4 class="no-margin color-white text-uppercase semi-bold width100">Uso de Paginación</h4>
+                    </div>
+                    <div class="grid-body">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input type="text" name="inputFilter" class="form-control" placeholder="Buscar">
+                                        <span class="input-group-addon  no-border">
+                                            <i class="fa fa-search"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="row m-t-10">
+                            <div class="col-md-12">
+                                 <table class="table table-bordered footable table-no-padding" data-filter="input[name=inputFilter]" data-page-size="8" data-limit-navigation="7">
+                            <thead>
+                                <tr>
+                                    <th style="width: 15%">DATO 1</th>
+                                    <th style="width: 30%">DATO 2</th>
+                                    <th style="width: 18%">DATO 3</th>
+                                    <th style="width: 10%">DATO 4</th>
+                                    <th style="width: 5%" class="text-center">HACER</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            	<tr>
+                                    <td>BUSCAR</td>
+                                    <td>DATOS</td>      
+                                    <td>CELDA</td>
+                                    <td>NUMERO</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20 pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                 <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>asas</td>
+                                    <td>sfasdf asdf</td>      
+                                    <td>asdasdsdf</td>
+                                    <td>asdasd</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>MANUEL</td>
+                                    <td>AGUILAR</td>      
+                                    <td>LOPEZ</td>
+                                    <td>1234567</td>
+                                    <td class="text-center">
+                                        <i class="fas fa-pencil-square-o i-20  tip pointer" data-toggle="tooltip" data-placement="left" title="Acción"></i>
+                                    </td>
+                                </tr>
+                            </tbody>
+                            <tfoot class="hide-if-no-paging">
+                                <tr>
+                                    <td colspan="6" class="text-center">
+                                        <ul class="pagination no-margin"></ul>
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        </table>  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
+        <!-- <script type="text/javascript" src="assets/js/bootstrap.min.js"></script> -->
+        <script type="text/javascript" src="assets/js/jquery.js"></script>
+        <script type="text/javascript" src="assets/js/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript" src="assets/footable/footable.all.min.js"></script>
+		<script type="text/javascript" src="assets/bootstrapv3/js/bootstrap.min.js"></script>
+		<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$('.footable').footable();
+				 $('[data-toggle="tooltip"]').tooltip()
+			});
+		</script>
+</body>
+</html>
